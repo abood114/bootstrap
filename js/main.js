00ddfed80898cee89b1d89c06d8b6e5cdfca38d5
Abdallah Saloul <96347155+abood114@users.noreply.github.com>
2023-05-28 10:48:09 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+var typed = new Typed('.typer', {
+    strings: ['Abdullah.','Designer.','Developer.'],
+    typspeed:100,
+    backspeed:100,
+    loop:true
+  });
+
+
+  const valueNumbers=document.querySelectorAll('.exp-number');
+  let interval=5000;
+
+  valueNumbers.forEach((valueNumber)=>{
+  let startValue=0;
+  let endValue=parseInt(valueNumber.getAttribute("data-val"));
+  let duration=Math.floor(interval/endValue);
+  let counter=setInterval(()=>{
+  startValue+=1;
+  valueNumber.textContent=startValue;
+  if(startValue==endValue){
+  clearInterval(counter);
+  }
+   },duration)
+  });
+
+  document.addEventListener("click",function(e){
+    if(e.target.classList.contains("gallery-item")){
+        const src=e.target.getAttribute("src");
+        document.querySelector(".modal-img").src=src;
+        const myModal=new bootstrap.Modal(document.getElementById('gallery-modal'))
+        myModal.show();
+    }
+
+  })
+
